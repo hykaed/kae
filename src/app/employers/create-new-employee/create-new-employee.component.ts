a774884f4972d5ceb4980e42c60b199f909e72ef
suelazeqo <suelazeqo19@gmail.com>
2022-03-16 14:05:40 +0100

    front end

--- src/app/employers/create-new-employee/create-new-employee.component.ts
+++ src/app/employers/create-new-employee/create-new-employee.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { EmployeeService } from '../employee.service';
 
 @Component({
   selector: 'app-create-new-employee',
@@ -7,11 +8,30 @@ import { FormGroup } from '@angular/forms';
   styleUrls: ['./create-new-employee.component.scss']
 })
 export class CreateNewEmployeeComponent implements OnInit {
-
+  editMode:boolean= false
   employeeForm:FormGroup;
-  constructor() { }
+  constructor(private employeeService:EmployeeService,
+              private fb: FormBuilder) { 
+              
+      this.employeeForm = this.fb.group({
+      employeeId:['',[Validators.required]],
+      name: ['', [Validators.required]],
+      lastName: ['', [Validators.required]],
+      jobPosition: ['', [Validators.required]],
+      salary: ['', [Validators.required]],
+              })  
+
+              }
 
   ngOnInit(): void {
+    this.employeeService.employeeSub.subscribe((response:any)=>{
+      this.editMode = response? true: false;
+      if(this.editMode){
+        this.employeeForm.patchValue(response);
+      }else{
+        this.employeeForm.reset();
+      }
+    })
   }
 
 }
