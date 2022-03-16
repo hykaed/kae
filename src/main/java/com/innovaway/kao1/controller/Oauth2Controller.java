e77c6b354210ca2198f474dc9d8c10950f22291a
Klajd Bakalli <klajdibakalli@gmail.com>
2022-03-16 14:17:01 +0100

    Spring Security + OAuth2

--- src/main/java/com/innovaway/kao1/controller/Oauth2Controller.java
+++ src/main/java/com/innovaway/kao1/controller/Oauth2Controller.java
@@ -2,7 +2,6 @@ package com.innovaway.kao1.controller;
 
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
