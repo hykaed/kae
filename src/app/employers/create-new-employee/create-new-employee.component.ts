b6a942642c12e520f09de8dec5918e2e8af112f1
suelazeqo <suelazeqo19@gmail.com>
2022-03-16 12:39:38 +0100

    first change

--- src/app/employers/create-new-employee/create-new-employee.component.ts
+++ src/app/employers/create-new-employee/create-new-employee.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-create-new-employee',
@@ -7,11 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateNewEmployeeComponent implements OnInit {
 
-
-
-
-
-  
+  employeeForm:FormGroup;
   constructor() { }
 
   ngOnInit(): void {
